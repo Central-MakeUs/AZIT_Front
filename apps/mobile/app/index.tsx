4622e7e6c7f599cd67a7d6572fb1537e9add6397
sandwe <preengus@gmail.com>
2025-12-28 21:05:10 +0900

    ✨ [feat] expo 기반 mobile app 추가

---
+++ apps/mobile/app/index.tsx
@@ -0,0 +1,12 @@
+import {Text} from "react-native";
+import {SafeAreaProvider, SafeAreaView} from "react-native-safe-area-context";
+
+export default function App() {
+  return (
+    <SafeAreaProvider style={{flex: 1}}>
+      <SafeAreaView style={{flex: 1}}>
+        <Text>Azit Application</Text>
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+}
