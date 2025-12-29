950bc36c54558843d9f05fc14ee38b97e492e1d8
sandwe <preengus@gmail.com>
2025-12-29 12:19:13 +0900

    ✨ [feat] typescript 공통 패키지 의존성 및 prettier 설정

--- apps/mobile/app/index.tsx
+++ apps/mobile/app/index.tsx
@@ -1,10 +1,10 @@
-import {Text} from "react-native";
-import {SafeAreaProvider, SafeAreaView} from "react-native-safe-area-context";
+import { Text } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <SafeAreaProvider style={{flex: 1}}>
-      <SafeAreaView style={{flex: 1}}>
+    <SafeAreaProvider style={{ flex: 1 }}>
+      <SafeAreaView style={{ flex: 1 }}>
         <Text>Azit Application</Text>
       </SafeAreaView>
     </SafeAreaProvider>
