4a3870c28099f371bfa5eca7539a64af0bdeb126
yummjin <cho4u4o@gmail.com>
2025-12-31 10:29:20 +0900

    ♻️ [refactor] variant 스타일링에 recipes 사용

--- packages/design-system/src/Button/button.css.ts
+++ packages/design-system/src/Button/button.css.ts
@@ -1,19 +1,25 @@
-import { style, styleVariants } from '@vanilla-extract/css';
 import { vars } from '../shared/config/vars.css';
+import { recipe } from '@vanilla-extract/recipes';
 
-const base = style({
-  borderRadius: 8,
-  border: 'none',
-  backgroundColor: vars.colors.blue40,
-});
-
-export const sizeVariant = styleVariants({
-  small: [base, { padding: 8 }],
-  medium: [base, { padding: 12 }],
-  large: [base, { padding: 16 }],
-});
-
-export const colorVariant = styleVariants({
-  primary: { backgroundColor: vars.colors.blue40 },
-  secondary: { backgroundColor: vars.colors.secondary },
+export const buttonVariant = recipe({
+  base: {
+    borderRadius: 8,
+    border: 'none',
+    backgroundColor: vars.colors.blue40,
+  },
+  variants: {
+    size: {
+      small: { padding: 8 },
+      medium: { padding: 12 },
+      large: { padding: 16 },
+    },
+    color: {
+      primary: { backgroundColor: vars.colors.blue40 },
+      secondary: { backgroundColor: vars.colors.secondary },
+    },
+  },
+  defaultVariants: {
+    size: 'medium',
+    color: 'primary',
+  },
 });
