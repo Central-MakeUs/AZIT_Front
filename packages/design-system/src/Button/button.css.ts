fcafdf9616b9647f77ceb7fc3a9fff57d78f9d67
yummjin <cho4u4o@gmail.com>
2025-12-31 10:45:44 +0900

    ♻️ [refactor] 컴포넌트의 style variant 타입을 style sheet를 통해 추출해 자동 적용하도록 변경

--- packages/design-system/src/Button/button.css.ts
+++ packages/design-system/src/Button/button.css.ts
@@ -1,6 +1,10 @@
 import { vars } from '../shared/config/vars.css';
 import { recipe } from '@vanilla-extract/recipes';
 
+export type ButtonVariants = NonNullable<Parameters<typeof buttonVariant>[0]>;
+export type ButtonSize = ButtonVariants['size'];
+export type ButtonColor = ButtonVariants['color'];
+
 export const buttonVariant = recipe({
   base: {
     borderRadius: 8,
