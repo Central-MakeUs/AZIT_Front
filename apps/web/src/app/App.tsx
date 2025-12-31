72f8dc355165b88a5f13c84b456699e70cabfe9c
sandwe <preengus@gmail.com>
2025-12-31 12:21:16 +0900

    ➕ [chore] stackflow 설치

--- apps/web/src/app/App.tsx
+++ apps/web/src/app/App.tsx
@@ -1,20 +1,22 @@
 import { useState } from 'react';
 import './styles/App.css';
 import { Button, vars } from '@azit/design-system';
+import { Stack } from './routes/stackflow';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h2>Web</h2>
+      {/* <h2>Web</h2>
       <Button
         label={`count is ${count}`}
         onClick={() => setCount((count) => count + 1)}
       />
       <p style={{ color: vars.colors.blue40 }}>
         Available to use design system in the web app.
-      </p>
+      </p> */}
+      <Stack />
     </>
   );
 }
