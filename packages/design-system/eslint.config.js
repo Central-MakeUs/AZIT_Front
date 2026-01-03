9d7d32b9976f19065dc8bef35b81ad41f01cf1f1
sandwe <preengus@gmail.com>
2026-01-03 16:46:28 +0900

    🚨 [fix] design-system 패키지 eslint 설정

---
+++ packages/design-system/eslint.config.js
@@ -0,0 +1,7 @@
+import { defineConfig, globalIgnores } from 'eslint/config';
+import { config as reactConfig } from '@azit/eslint/react';
+
+export default defineConfig([
+  globalIgnores(['dist', 'storybook-static']),
+  ...reactConfig,
+]);
