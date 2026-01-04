2794ff9d571f96dc79db5d31dbe0b2622a21615f
yummjin <cho4u4o@gmail.com>
2026-01-04 20:13:37 +0900

    🔧 [chore] 이모지 설정 파일로 통일

---
+++ .commit-types.js
@@ -0,0 +1,13 @@
+module.exports = {
+  TYPES: ['feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test', 'chore'],
+  EMOJIS: {
+    feat: '✨',
+    fix: '🐛',
+    docs: '📝',
+    style: '💄',
+    refactor: '♻️',
+    perf: '⚡',
+    test: '✅',
+    chore: '🔧',
+  },
+};
