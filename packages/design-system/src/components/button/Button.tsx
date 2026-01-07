e7d1d38b8cc670fb8603053ce15c515d9b78904c
yummjin <cho4u4o@gmail.com>
2026-01-07 17:04:29 +0900

    ✨ [feat] 버튼 컴포넌트에 클릭 핸들러 및 클래스 prop 추가

--- packages/design-system/src/components/button/Button.tsx
+++ packages/design-system/src/components/button/Button.tsx
@@ -4,6 +4,7 @@ import { buttonVariant, type ButtonSize, type ButtonColor } from './button.css';
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   size?: ButtonSize;
   color?: ButtonColor;
+  className?: string;
   children: ReactNode;
   onClick?: () => void;
 }
@@ -12,11 +13,18 @@ export default function Button({
   size,
   color,
   children,
+  className,
   type = 'button',
+  onClick,
   ...props
 }: ButtonProps) {
   return (
-    <button type={type} className={buttonVariant({ size, color })} {...props}>
+    <button
+      type={type}
+      className={buttonVariant({ size, color }) + ' ' + className}
+      onClick={onClick}
+      {...props}
+    >
       {children}
     </button>
   );
