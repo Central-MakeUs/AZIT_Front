873ee082f116e73a534112735741ff939ff99e6d
sandwe <preengus@gmail.com>
2026-01-07 16:32:26 +0900

    💄 [style] 칩 컴포넌트 추가

---
+++ packages/design-system/src/components/chip/Chip.tsx
@@ -0,0 +1,15 @@
+import type { HTMLAttributes, ReactNode } from 'react';
+import { chipVariant, type ChipType } from './chip.css';
+
+export interface ChipProps extends HTMLAttributes<HTMLSpanElement> {
+  type?: ChipType;
+  children: ReactNode;
+}
+
+export default function Chip({ type, children, ...props }: ChipProps) {
+  return (
+    <span className={chipVariant({ type })} {...props}>
+      {children}
+    </span>
+  );
+}
