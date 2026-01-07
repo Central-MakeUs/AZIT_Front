82275e55ef5cb1bfa893aef4b66c6018f683fee3
sandwe <preengus@gmail.com>
2026-01-07 16:32:26 +0900

    💄 [style] 칩 컴포넌트 추가

---
+++ packages/design-system/src/components/chip/chip.css.ts
@@ -0,0 +1,35 @@
+import { vars } from '../../shared/config/vars.css';
+import { recipe } from '@vanilla-extract/recipes';
+
+export type ChipVariants = NonNullable<Parameters<typeof chipVariant>[0]>;
+export type ChipType = ChipVariants['type'];
+
+export const chipVariant = recipe({
+  base: {
+    display: 'inline-flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: '10px',
+    padding: '4px 8px',
+    borderRadius: '8px',
+    border: 'none',
+    fontFamily: vars.typography.fontFamily.primary,
+    ...vars.typography.body.b4,
+    textAlign: 'center',
+  },
+  variants: {
+    type: {
+      primary: {
+        backgroundColor: vars.colors.blue60,
+        color: vars.colors.white,
+      },
+      secondary: {
+        backgroundColor: vars.colors.secondary,
+        color: vars.colors.black,
+      },
+    },
+  },
+  defaultVariants: {
+    type: 'primary',
+  },
+});
