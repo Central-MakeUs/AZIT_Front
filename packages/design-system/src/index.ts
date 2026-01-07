ebbdcc83f196151c761fa7f4f2847403fc91f980
sandwe <preengus@gmail.com>
2026-01-07 16:32:26 +0900

    💄 [style] 칩 컴포넌트 추가

--- packages/design-system/src/index.ts
+++ packages/design-system/src/index.ts
@@ -1,3 +1,4 @@
 export { default as Button } from './components/button/Button';
+export { default as Chip } from './components/chip/Chip';
 export { vars } from './shared/config/vars.css';
 import './shared/styles/fonts.css';
