a7579a48ed44a568b887de6b317f581ad460e1d5
sandwe <preengus@gmail.com>
2026-01-09 18:38:39 +0900

    ✨ [feat] 아이콘 컴포넌트 추가

---
+++ packages/design-system/src/components/icon/MarkerPin04Icon.tsx
@@ -0,0 +1,34 @@
+import { forwardRef } from 'react';
+import type { Ref } from 'react';
+import { clsx } from 'clsx';
+import { iconVariant } from './icon.css';
+import type { IconProps } from './types';
+
+function MarkerPin04Icon(
+  { size = 24, color, className, ...props }: IconProps,
+  ref: Ref<SVGSVGElement>
+) {
+  return (
+    <svg
+      viewBox="0 0 22 22"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      ref={ref}
+      className={clsx(iconVariant({ color }), className)}
+      {...props}
+    >
+      <path
+        d="M4 13.2864C2.14864 14.1031 1 15.2412 1 16.5C1 18.9853 5.47715 21 11 21C16.5228 21 21 18.9853 21 16.5C21 15.2412 19.8514 14.1031 18 13.2864M17 7C17 11.0637 12.5 13 11 16C9.5 13 5 11.0637 5 7C5 3.68629 7.68629 1 11 1C14.3137 1 17 3.68629 17 7ZM12 7C12 7.55228 11.5523 8 11 8C10.4477 8 10 7.55228 10 7C10 6.44772 10.4477 6 11 6C11.5523 6 12 6.44772 12 7Z"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
+
+const ForwardRef = forwardRef(MarkerPin04Icon);
+export default ForwardRef;
