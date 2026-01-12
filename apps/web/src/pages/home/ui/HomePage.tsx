b2f733984dd35b7485b8ca8e99b19cbe860d72bc
yummjin <cho4u4o@gmail.com>
2026-01-12 18:28:37 +0900

    💄 [style] 버튼 컴포넌트 스타일 수정 및 코드 정리

--- apps/web/src/pages/home/ui/HomePage.tsx
+++ apps/web/src/pages/home/ui/HomePage.tsx
@@ -1,5 +1,4 @@
 import { Button, vars } from '@azit/design-system';
-import '@/app/styles/App.css';
 import { useFlow } from '@/app/routes/stackflow';
 import { AppScreen } from '@stackflow/plugin-basic-ui';
 
@@ -9,10 +8,9 @@ export function HomePage() {
   return (
     <AppScreen>
       <h2>Hello World</h2>
-      <Button
-        label={'로그인 페이지로 이동'}
-        onClick={() => push('LoginPage', {}, { animate: false })}
-      />
+      <Button onClick={() => push('LoginPage', {}, { animate: false })}>
+        로그인 페이지로 이동
+      </Button>
       <p style={{ color: vars.colors.blue40 }}>
         Available to use design system in the web app.
       </p>
