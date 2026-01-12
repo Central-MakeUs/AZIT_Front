f62d8a632c604e62f3706436c07588c4e04680b0
yummjin <cho4u4o@gmail.com>
2026-01-12 18:27:25 +0900

    ✨ [feat] 로그인 페이지 UI 구현 및 버튼 스타일 추가

---
+++ apps/web/src/pages/auth/styles/loginPage.css.ts
@@ -0,0 +1,49 @@
+import { container } from '@/shared/styles/container.css';
+import { vars } from '@azit/design-system';
+import { composeStyles, style } from '@vanilla-extract/css';
+
+export const loginContainer = composeStyles(
+  container,
+  style({
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'relative',
+  })
+);
+
+export const titleWrapper = style({
+  position: 'absolute',
+  top: 280,
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+});
+
+export const title = style({
+  fontSize: 64,
+  fontWeight: 600,
+  lineHeight: 1.4,
+  color: vars.colors.white,
+  fontFamily: 'Tilt Warp',
+  letterSpacing: '2.5px',
+});
+
+export const description = style({
+  ...vars.typography.body.b2,
+  color: vars.colors.white,
+});
+
+export const buttonWrapper = style({
+  position: 'absolute',
+  left: 20,
+  right: 20,
+  bottom: 100,
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  gap: 12,
+  marginTop: 185,
+});
