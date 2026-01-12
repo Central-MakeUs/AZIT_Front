d3204675b56941454b486306fd1785da9ba3a931
yummjin <cho4u4o@gmail.com>
2026-01-12 17:37:35 +0900

    ✨ [feat] 디자인 시스템 컴포넌트로 변경 및 스타일 시트 추가

--- apps/web/src/app/main.tsx
+++ apps/web/src/app/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './styles/index.css';
-import App from './App.tsx';
+import '@azit/design-system';
+import { Stack } from './routes/stackflow';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Stack />
   </StrictMode>
 );
