46f4469d4ae35aab769de82703e43d98cfb0ab11
sandwe <preengus@gmail.com>
2026-01-14 20:19:52 +0900

    ✨ [feat] zustand 설치 및 accessToken 헤더 공통 처리

--- apps/web/src/shared/lib/ky.ts
+++ apps/web/src/shared/lib/ky.ts
@@ -1,12 +1,25 @@
 import ky from 'ky';
+import { useAuthStore } from './zustand';
 
 export const api = ky.create({
   prefixUrl: import.meta.env.VITE_API_URL,
   credentials: 'include',
-  // 기본값 명시
+  // Default Options
   headers: {
     'Content-Type': 'application/json',
   },
   timeout: 10000,
   retry: 2,
+  // Interceptor Hooks
+  hooks: {
+    beforeRequest: [
+      (request) => {
+        const { accessToken } = useAuthStore.getState();
+
+        if (accessToken) {
+          request.headers.set('Authorization', `Bearer ${accessToken}`);
+        }
+      },
+    ],
+  },
 });
