3cb50604a815a55e28735ed803737f6ffaf5355b
sandwe <preengus@gmail.com>
2026-01-14 22:23:21 +0900

    ✨ [feat] 401 에러 시 토큰 재발급 로직 추가

--- apps/web/src/shared/lib/ky.ts
+++ apps/web/src/shared/lib/ky.ts
@@ -1,5 +1,10 @@
 import ky from 'ky';
 import { useAuthStore } from './zustand';
+import type { components } from '../api/apiTypes';
+import type { ApiResponse } from '../api/baseTypes';
+
+type ReissueTokenResult = components['schemas']['SocialLoginResponse'];
+type ReissueTokenResponse = ApiResponse<ReissueTokenResult>;
 
 export const api = ky.create({
   prefixUrl: import.meta.env.VITE_API_URL,
@@ -18,6 +23,28 @@ export const api = ky.create({
 
         if (accessToken) {
           request.headers.set('Authorization', `Bearer ${accessToken}`);
+        }
+      },
+    ],
+    afterResponse: [
+      // 401 에러 발생시, 새 토큰으로 재시도
+      async (request, _options, response, state) => {
+        if (response.status === 401 && state.retryCount === 0) {
+          const {
+            result: { accessToken },
+          } = await ky
+            .post(`${import.meta.env.VITE_API_URL}/auth/reissue`, {
+              credentials: 'include',
+            })
+            .json<ReissueTokenResponse>();
+
+          const headers = new Headers(request.headers);
+          headers.set('Authorization', `Bearer ${accessToken}`);
+
+          return ky.retry({
+            request: new Request(request, { headers }),
+            code: 'TOKEN_REFRESHED',
+          });
         }
       },
     ],
