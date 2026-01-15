fc6164399d43153cdccf3c295db9418fc60a092c
sandwe <preengus@gmail.com>
2026-01-15 11:09:21 +0900

    ✨ [feat] vite 환경 변수 타입 정의 추가

--- apps/web/src/pages/store/api/storeQueries.ts
+++ apps/web/src/pages/store/api/storeQueries.ts
@@ -1,5 +1,5 @@
 import { queryOptions } from '@tanstack/react-query';
-import { getStoreProducts } from './getStoreProducts';
+// import { getStoreProducts } from './getStoreProducts';
 
 // query factory
 export const storeQueries = {
