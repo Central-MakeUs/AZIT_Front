d204a8bbc8ef2b31393acaa381337b007b2e366f
sandwe <preengus@gmail.com>
2026-01-16 17:30:54 +0900

    ♻️ [refactor] 엔드포인트 파일 분리 및 openapi type model 정의

--- apps/web/src/features/auth/api/authTypes.ts
+++ apps/web/src/features/auth/api/authTypes.ts
@@ -1,9 +1,10 @@
-import type { components, operations } from '@/shared/api/apiTypes';
 import type { ApiResponse } from '@/shared/api/baseTypes';
+import type {
+  AuthProvider,
+  SocialLoginRequest,
+  SocialLoginResult,
+} from '@/shared/api/models';
 
-export type AuthProvider =
-  operations['socialLogin']['parameters']['path']['provider'];
-export type SocialLoginRequest = components['schemas']['SocialLoginRequest'];
-export type SocialLoginResult = components['schemas']['SocialLoginResponse'];
-
-export type SocialLoginApiResponse = ApiResponse<SocialLoginResult>;
+export type AuthProviderType = AuthProvider;
+export type SocialLoginRequestType = ApiResponse<SocialLoginRequest>;
+export type SocialLoginResponseType = ApiResponse<SocialLoginResult>;
