19958c2322de705739a1680ce773d6249ca86f80
sandwe <preengus@gmail.com>
2026-01-18 13:10:08 +0900

    ✨ [feat] Checkbox 디자인 시스템 UI 추가

---
+++ packages/design-system/src/components/checkbox/Checkbox.stories.ts
@@ -0,0 +1,51 @@
+import type { Meta, StoryObj } from '@storybook/react-vite';
+
+import Checkbox from './Checkbox';
+
+const meta = {
+  title: 'Components/Checkbox',
+  component: Checkbox,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    checked: {
+      control: 'boolean',
+    },
+    disabled: {
+      control: 'boolean',
+    },
+    onCheckedChange: {
+      action: 'checked-change',
+    },
+  },
+} satisfies Meta<typeof Checkbox>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    defaultChecked: false,
+  },
+};
+
+export const Checked: Story = {
+  args: {
+    defaultChecked: true,
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    defaultChecked: false,
+    disabled: true,
+  },
+};
+
+export const Indeterminate: Story = {
+  args: {
+    checked: 'indeterminate',
+  },
+};
---
+++ packages/design-system/src/components/checkbox/Checkbox.tsx
@@ -0,0 +1,32 @@
+import * as CheckboxPrimitive from '@radix-ui/react-checkbox';
+import { forwardRef } from 'react';
+import type { ComponentPropsWithoutRef, Ref } from 'react';
+import { clsx } from 'clsx';
+import { CheckIcon } from '../icon';
+import { checkboxRoot, checkboxIndicator } from './checkbox.css';
+
+export interface CheckboxProps extends ComponentPropsWithoutRef<
+  typeof CheckboxPrimitive.Root
+> {
+  className?: string;
+}
+
+function Checkbox(
+  { className, ...props }: CheckboxProps,
+  ref: Ref<HTMLButtonElement>
+) {
+  return (
+    <CheckboxPrimitive.Root
+      ref={ref}
+      className={clsx(checkboxRoot, className)}
+      {...props}
+    >
+      <CheckboxPrimitive.Indicator className={checkboxIndicator}>
+        <CheckIcon size={16} />
+      </CheckboxPrimitive.Indicator>
+    </CheckboxPrimitive.Root>
+  );
+}
+
+const ForwardRef = forwardRef(Checkbox);
+export { ForwardRef as Checkbox };
