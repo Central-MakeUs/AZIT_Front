d313916d74a066e26113d5289a5e9a57195bc75a
sandwe <preengus@gmail.com>
2026-01-18 13:13:36 +0900

    ✨ [feat] MinusIcon 컴포넌트 추가 및 기존 아이콘 수정

--- packages/design-system/src/components/icon/ChevronRightIcon.tsx
+++ packages/design-system/src/components/icon/ChevronRightIcon.tsx
@@ -10,7 +10,7 @@ function ChevronRightIcon(
 ) {
   return (
     <svg
-      viewBox="0 0 8 14"
+      viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       width={size}
@@ -20,7 +20,7 @@ function ChevronRightIcon(
       {...props}
     >
       <path
-        d="M1 13L7 7L1 1"
+        d="M9 18L15 12L9 6"
         stroke="currentColor"
         strokeWidth="2"
         strokeLinecap="round"
