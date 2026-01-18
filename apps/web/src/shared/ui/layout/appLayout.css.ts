e8f416224ddebdd262bb7d0bde7167e09ebc9879
sandwe <preengus@gmail.com>
2026-01-18 16:50:41 +0900

    ✨ [feat] BackButton 공통 컴포넌트 추가

--- apps/web/src/shared/ui/layout/appLayout.css.ts
+++ apps/web/src/shared/ui/layout/appLayout.css.ts
@@ -4,6 +4,8 @@ export const appLayout = style({
   display: 'flex',
   flexDirection: 'column',
   alignItems: 'center',
-  width: 'full',
+  width: '100%',
+  height: '100%',
   paddingBottom: 32,
+  position: 'relative',
 });
