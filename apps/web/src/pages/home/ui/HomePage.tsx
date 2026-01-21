b14de332fbcd428f5ba93b95556e9f355770b39c
sandwe <preengus@gmail.com>
2026-01-22 01:10:17 +0900

    ✨ [feat] 최초 인증 상태별 리다이렉트 시 기존 페이지 깜빡임 방지

--- apps/web/src/pages/home/ui/HomePage.tsx
+++ apps/web/src/pages/home/ui/HomePage.tsx
@@ -1,16 +1,9 @@
-import { Button, vars } from '@azit/design-system';
-import { useFlow } from '@/app/routes/stackflow';
+import { vars } from '@azit/design-system';
 import { AppScreen } from '@stackflow/plugin-basic-ui';
 
 export function HomePage() {
-  const { push } = useFlow();
-
   return (
     <AppScreen>
-      <h2>Hello World</h2>
-      <Button onClick={() => push('LoginPage', {}, { animate: false })}>
-        로그인 페이지로 이동
-      </Button>
       <p style={{ color: vars.colors.blue40 }}>
         Available to use design system in the web app.
       </p>
