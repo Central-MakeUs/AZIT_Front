d3549e73cefe294363789022cc144492595384d9
sandwe <preengus@gmail.com>
2026-01-21 21:28:40 +0900

    ✨ [feat] 분리한 api 인스턴스 적용 및 약관동의 API 추가

--- apps/web/src/features/auth/api/postReissueToken.ts
+++ apps/web/src/features/auth/api/postReissueToken.ts
@@ -1,15 +1,13 @@
-import ky from 'ky';
-
 import type { ApiResponse } from '@/shared/api/baseTypes';
 import type { ReissueTokenResult } from '@/shared/api/models';
-import { BASE_API_URL } from '@/shared/constants/url';
 import { END_POINT } from '@/shared/constants/endpoint';
+import { baseApi } from '@/shared/api/apiClient';
 
 type ReissueTokenResponse = ApiResponse<ReissueTokenResult>;
 
 export const postReissueToken = async (): Promise<ReissueTokenResponse> => {
-  const response = await ky
-    .post(`${BASE_API_URL}${END_POINT.AUTH.REISSUE_TOKEN}`, {
+  const response = await baseApi
+    .post(END_POINT.AUTH.REISSUE_TOKEN, {
       credentials: 'include',
     })
     .json<ReissueTokenResponse>();
