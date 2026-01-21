da0921b3fe0908ff3d17077a3360919f7a5beb53
sandwe <preengus@gmail.com>
2026-01-21 21:29:19 +0900

    ✨ [feat] 약관 동의 API 연동

--- apps/web/src/pages/auth/ui/TermAgreePage.tsx
+++ apps/web/src/pages/auth/ui/TermAgreePage.tsx
@@ -1,7 +1,22 @@
+import { useFlow } from '@/app/routes/stackflow';
+import { postTermAgree } from '@/pages/auth/api/postTermAgree';
+import { Button } from '@azit/design-system';
+
 export function TermAgreePage() {
+  const { replace } = useFlow();
+  const handleTermAgree = async () => {
+    try {
+      await postTermAgree();
+      replace('OnboardingPage', {}, { animate: false });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div>
       <h1>TermAgreePage</h1>
+      <Button onClick={handleTermAgree}>다음</Button>
     </div>
   );
 }
