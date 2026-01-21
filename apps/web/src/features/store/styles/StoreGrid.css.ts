180bd5f425d1917a9793f1c5ed6cc1551515bca9
yummjin <cho4u4o@gmail.com>
2026-01-21 12:40:41 +0900

    💄 [style] grid container 스타일시트 flexbox에서 grid로 변경

--- apps/web/src/features/store/styles/StoreGrid.css.ts
+++ apps/web/src/features/store/styles/StoreGrid.css.ts
@@ -1,9 +1,10 @@
 import { style } from '@vanilla-extract/css';
 
 export const gridContainer = style({
-  display: 'flex',
+  display: 'grid',
   flexWrap: 'wrap',
   gap: '12px 11px',
   alignItems: 'flex-start',
   width: '100%',
+  gridTemplateColumns: 'repeat(2, 1fr)',
 });
