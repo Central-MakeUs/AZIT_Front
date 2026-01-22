331242644cf2a1034b2196ee1d4ba41ba6801657
yummjin <cho4u4o@gmail.com>
2026-01-22 15:08:29 +0900

    ✨ [feat] 카카오 로그인 hook 통합 및 SDK 상수 추가

--- apps/web/src/shared/constants/url.ts
+++ apps/web/src/shared/constants/url.ts
@@ -1,2 +1,4 @@
 export const BASE_API_URL = import.meta.env.VITE_API_URL;
 export const APPLE_AUTHORIZE_URL = import.meta.env.VITE_APPLE_AUTHORIZE_URL;
+export const KAKAO_AUTHORIZE_URL = import.meta.env.VITE_KAKAO_AUTHORIZE_URL;
+export const KAKAO_JS_SDK_KEY = import.meta.env.VITE_KAKAO_JS_SDK_KEY;
