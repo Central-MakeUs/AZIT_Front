ba26e5fa47478c122df16730e79fc59b08e0cf90
yummjin <cho4u4o@gmail.com>
2026-01-22 15:00:44 +0900

    ♻️ [refactor] `withAuth` 주석화

--- apps/web/src/app/routes/withAuth.tsx
+++ apps/web/src/app/routes/withAuth.tsx
@@ -1,6 +1,6 @@
 import type { ComponentType } from 'react';
-import { useFlow } from '@/app/routes/stackflow';
-import { useAuthStore } from '@/shared/store/auth';
+// import { useFlow } from '@/app/routes/stackflow';
+// import { useAuthStore } from '@/shared/store/auth';
 
 /**
  * 회원 인증이 필요한 페이지를 보호하는 HOC입니다.
@@ -8,13 +8,13 @@ import { useAuthStore } from '@/shared/store/auth';
  */
 export function withAuth<T extends object>(Component: ComponentType<T>) {
   return function AuthenticatedComponent(props: T) {
-    const { replace } = useFlow();
+    // const { replace } = useFlow();
 
-    const { accessToken } = useAuthStore.getState();
-    if (!accessToken) {
-      replace('LoginPage', {}, { animate: false });
-      return <></>;
-    }
+    // const { accessToken } = useAuthStore.getState();
+    // if (!accessToken) {
+    //   replace('LoginPage', {}, { animate: false });
+    //   return <></>;
+    // }
 
     // 인증 토큰이 있으면 원본 컴포넌트 렌더링
     return <Component {...props} />;
