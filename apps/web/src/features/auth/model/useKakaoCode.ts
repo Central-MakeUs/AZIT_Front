4176c2a05f928fea6ed5a5a71c7e6e7890c4ac0f
yummjin <cho4u4o@gmail.com>
2026-01-23 16:32:36 +0900

    ✨ [feat] 소셜 로그인 응답 타입 개선 및 카카오 로그인 로직 수정

--- apps/web/src/features/auth/model/useKakaoCode.ts
+++ apps/web/src/features/auth/model/useKakaoCode.ts
@@ -1,11 +1,16 @@
 import { useEffect, useState } from 'react';
 
 import { postSocialLogin } from '../api/postSocialLogin';
+import { useAuthStore } from '@/shared/store/auth';
+import { useFlow } from '@/app/routes/stackflow';
 
 export const useKakaoCode = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(false);
   const [invalidAccess, setInvalidAccess] = useState(false);
+
+  const { setAccessToken } = useAuthStore();
+  const { replace } = useFlow();
 
   useEffect(() => {
     const code = new URLSearchParams(window.location.search).get('code');
@@ -15,15 +20,32 @@ export const useKakaoCode = () => {
     } else {
       setInvalidAccess(true);
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const login = async (code: string) => {
     setIsLoading(true);
 
     try {
-      await postSocialLogin('KAKAO', {
+      const {
+        result: { accessToken, status },
+      } = await postSocialLogin('KAKAO', {
         authorizationCode: code,
       });
+
+      setAccessToken(accessToken);
+
+      switch (status) {
+        case 'PENDING_TERMS':
+          replace('TermAgreePage', {}, { animate: false });
+          break;
+        case 'PENDING_ONBOARDING':
+          replace('OnboardingPage', {}, { animate: false });
+          break;
+        case 'ACTIVE':
+          replace('HomePage', {}, { animate: false });
+          break;
+      }
     } catch (err) {
       setError(true);
       console.error('Login failed:', err);
