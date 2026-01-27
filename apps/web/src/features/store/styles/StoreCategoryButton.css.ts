fa2d44dd6c8f75563da3cec3b429bb4ef6ea2ff3
yummjin <cho4u4o@gmail.com>
2026-01-27 19:11:35 +0900

    ♻️ [refactor] typography 모듈을 사용하여 스타일 개선

--- apps/web/src/features/store/styles/StoreCategoryButton.css.ts
+++ apps/web/src/features/store/styles/StoreCategoryButton.css.ts
@@ -1,5 +1,5 @@
 import { style } from '@vanilla-extract/css';
-import { vars } from '@azit/design-system';
+import { vars, typography } from '@azit/design-system';
 
 export const categoryButton = style({
   display: 'flex',
@@ -11,10 +11,10 @@ export const categoryButton = style({
   cursor: 'pointer',
 });
 
-export const categoryButtonText = style({
-  fontSize: vars.typography.body.b3.fontSize,
-  fontWeight: vars.typography.body.b3.fontWeight,
-  lineHeight: vars.typography.body.b3.lineHeight,
-  color: vars.colors.white,
-  textAlign: 'center',
-});
+export const categoryButtonText = style([
+  typography.body.b3,
+  {
+    color: vars.colors.white,
+    textAlign: 'center',
+  },
+]);
