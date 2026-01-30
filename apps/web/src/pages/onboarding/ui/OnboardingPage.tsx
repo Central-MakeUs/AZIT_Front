cbf86d2f073acfb0f03dfc08bc8e4ab8ee90ee6e
sandwe <preengus@gmail.com>
2026-01-30 13:22:15 +0900

    ✨ [feat] 리더 온보딩 마크업 구현

--- apps/web/src/pages/onboarding/ui/OnboardingPage.tsx
+++ apps/web/src/pages/onboarding/ui/OnboardingPage.tsx
@@ -1,29 +1,95 @@
-import { Button } from '@azit/design-system';
-import { postCreateCrew } from '@/features/onboarding/api/postCreateCrew';
-import { useFlow } from '@/app/routes/stackflow';
+import { useState } from 'react';
+import { AppScreen } from '@stackflow/plugin-basic-ui';
+import { AppLayout } from '@/shared/ui/layout';
+import { useFunnel } from '@/features/onboarding/hooks/useFunnel';
+import { OnboardingRoleSelect } from '@/features/onboarding/ui/OnboardingRoleSelect';
+import {
+  OnboardingCrewName,
+  OnboardingCrewRegion,
+  OnboardingShareInviteCode,
+} from '@/features/onboarding/ui';
+import { Header } from '@azit/design-system';
+import { BackButton } from '@/shared/ui/button';
+
+type StepName =
+  | 'role-select'
+  | 'crew-name'
+  | 'crew-region'
+  | 'share-invite-code'
+  | 'member-join'
+  | 'enter-invite-code'
+  | 'request-complete';
+
+const ONBOARDING_FLOW = {
+  'role-select': {
+    leader: 'crew-name',
+    member: 'member-join',
+  },
+  'crew-name': 'crew-region',
+  'crew-region': 'share-invite-code',
+  'share-invite-code': null,
+  'member-join': 'enter-invite-code',
+  'enter-invite-code': 'request-complete',
+  'request-complete': null,
+} as const;
+
+const initialStep = 'role-select';
+const hideBackButton = ['role-select', 'share-invite-code'];
 
 export function OnboardingPage() {
-  const { replace } = useFlow();
+  const [Funnel, step, setStep] = useFunnel<StepName>(initialStep);
 
-  const handleCreateCrew = async () => {
-    try {
-      await postCreateCrew({
-        name: 'test',
-        category: 'RUNNING',
-        region: 'SEOUL',
-      });
+  const [history, setHistory] = useState<StepName[]>([]);
+  const showBackButton = !hideBackButton.includes(step);
 
-      // TODO: 크루 초대 코드 발급 화면의 버튼에서 처리, 현재는 테스트용
-      replace('HomePage', {}, { animate: false });
-    } catch (error) {
-      console.error(error);
-    }
+  const onBack = () => {
+    if (history.length === 0) return;
+
+    setStep(history[history.length - 1]);
+    setHistory((prev) => prev.slice(0, -1));
+  };
+
+  const onNext = (action: () => void) => {
+    action();
+    setHistory((prev) => [...prev, step]);
   };
 
   return (
-    <div>
-      <h1>OnboardingPage</h1>
-      <Button onClick={handleCreateCrew}>크루 생성하기</Button>
-    </div>
+    <AppScreen>
+      <AppLayout>
+        {showBackButton && (
+          <Header sticky left={<BackButton onClick={onBack} />} />
+        )}
+        <Funnel>
+          <Funnel.Step name="role-select">
+            <OnboardingRoleSelect
+              onNext={(role) =>
+                onNext(() =>
+                  setStep(role === 'leader' ? 'crew-name' : 'member-join')
+                )
+              }
+            />
+          </Funnel.Step>
+          <Funnel.Step name="crew-name">
+            <OnboardingCrewName
+              onNext={() => onNext(() => setStep('crew-region'))}
+            />
+          </Funnel.Step>
+          <Funnel.Step name="crew-region">
+            <OnboardingCrewRegion
+              onNext={() => onNext(() => setStep('share-invite-code'))}
+            />
+          </Funnel.Step>
+          <Funnel.Step name="share-invite-code">
+            <OnboardingShareInviteCode
+              crewName="test"
+              crewProfileImage="example.png"
+              inviteCode="123456"
+            />
+          </Funnel.Step>
+          {/* TODO: 멤버 온보딩 UI 추가 */}
+        </Funnel>
+      </AppLayout>
+    </AppScreen>
   );
 }
