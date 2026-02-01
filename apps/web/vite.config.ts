1598dcf964938a974722a30014d31d5dc83e3091
sandwe <preengus@gmail.com>
2026-02-02 01:36:14 +0900

    🔧 [chore] 로컬 https 설정

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import fs from 'fs';
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 
 // https://vite.dev/config/
@@ -13,6 +14,10 @@ export default defineConfig({
   },
   server: {
     host: '0.0.0.0',
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, '../../localhost-key.pem')),
+      cert: fs.readFileSync(path.resolve(__dirname, '../../localhost.pem')),
+    },
     port: 5173,
   },
 });
