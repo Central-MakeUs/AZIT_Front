20228fee5c43c82745af8c18faa91bb244572097
sandwe <preengus@gmail.com>
2026-02-03 19:29:15 +0900

    ✨ [feat] 크루 온보딩 관련 API 호출 함수 추가

--- apps/web/src/shared/api/models.ts
+++ apps/web/src/shared/api/models.ts
@@ -8,3 +8,5 @@ export type ReissueTokenResult = components['schemas']['SocialLoginResponse'];
 export type TermAgreeRequest = components['schemas']['AgreeToTermsRequest'];
 export type CreateCrewRequest = components['schemas']['CreateCrewRequest'];
 export type CreateCrewResult = components['schemas']['CreateCrewResponse'];
+export type JoinCrewRequest = components['schemas']['JoinCrewRequest'];
+export type CrewInfoResult = components['schemas']['CrewInvitationResponse'];
