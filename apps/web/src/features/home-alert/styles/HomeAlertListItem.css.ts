58e68a61a1c7bde6c2fd2cf29f36d81535d96127
yummjin <cho4u4o@gmail.com>
2026-02-05 14:39:58 +0900

    💄 [style] 알림 페이지 탭, 리스트 아이템 미세 스타일 조정

--- apps/web/src/features/home-alert/styles/HomeAlertListItem.css.ts
+++ apps/web/src/features/home-alert/styles/HomeAlertListItem.css.ts
@@ -13,10 +13,14 @@ export const itemContainer = style({
   border: `1px solid ${vars.colors.gray10}`,
 });
 
+export const itemContainerUnread = style({
+  border: `1px solid ${vars.colors.blue60}`,
+});
+
 export const contentContainer = style({
   display: 'flex',
   flexDirection: 'column',
-  gap: '4px',
+  gap: '6px',
   flex: 1,
   minWidth: 0,
 });
@@ -29,7 +33,7 @@ export const mainText = style({
 });
 
 export const dateText = style([
-  typography.body.b2,
+  typography.body.b3,
   {
     color: vars.colors.black,
   },
@@ -38,14 +42,14 @@ export const dateText = style([
 export const descriptionText = style([
   typography.body.b4,
   {
-    color: vars.colors.gray50,
+    color: vars.colors.gray60,
   },
 ]);
 
 export const timestampText = style([
   typography.body.b4,
   {
-    color: vars.colors.gray50,
+    color: vars.colors.gray60,
     flexShrink: 0,
   },
 ]);
