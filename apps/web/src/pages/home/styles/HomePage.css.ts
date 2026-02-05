c5593a2f9bb61693aac69d3d7509016e8a631689
yummjin <cho4u4o@gmail.com>
2026-02-05 15:25:51 +0900

    ♻️ [refactor] 홈 화면 및 일정 화면 동일 레이아웃 적용

--- apps/web/src/pages/home/styles/HomePage.css.ts
+++ apps/web/src/pages/home/styles/HomePage.css.ts
@@ -1,27 +1,4 @@
 import { style } from '@vanilla-extract/css';
-import { vars, typography } from '@azit/design-system';
-
-export const pageContainer = style({
-  display: 'flex',
-  flexDirection: 'column',
-  width: '100%',
-});
-
-export const scheduleSection = style({
-  display: 'flex',
-  flexDirection: 'column',
-  gap: '10px',
-  width: '100%',
-  backgroundColor: vars.colors.background_sub,
-  padding: '20px',
-});
-
-export const sectionTitle = style([
-  typography.body.b1,
-  {
-    color: vars.colors.black,
-  },
-]);
 
 export const scheduleList = style({
   display: 'flex',
---
+++ apps/web/src/widgets/schedule-section-layout/styles/ScheduleSectionLayout.css.ts
@@ -0,0 +1,34 @@
+import { style } from '@vanilla-extract/css';
+import { vars, typography } from '@azit/design-system';
+
+export const pageContainer = style({
+  display: 'flex',
+  flexDirection: 'column',
+  width: '100%',
+});
+
+export const topSection = style({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  width: '100%',
+  padding: '20px',
+  backgroundColor: vars.colors.white,
+  minHeight: 330,
+});
+
+export const scheduleSection = style({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '10px',
+  width: '100%',
+  backgroundColor: vars.colors.background_sub,
+  padding: '20px',
+});
+
+export const sectionTitle = style([
+  typography.body.b1,
+  {
+    color: vars.colors.black,
+  },
+]);
