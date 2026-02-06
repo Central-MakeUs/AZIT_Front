3333087fa72e30b818f8e9aaf49ce77ec3337e50
sandwe <preengus@gmail.com>
2026-02-06 23:27:34 +0900

    ♻️ [refactor] api 인스턴스 바인딩 기반 http 메소드 생성 함수 구현

--- apps/web/src/pages/store/api/getStoreProducts.ts
+++ apps/web/src/pages/store/api/getStoreProducts.ts
@@ -1,8 +1,7 @@
-import { authApi } from '@/shared/api/apiClient';
-import { get } from '@/shared/api/httpMethods';
+import { auth } from '@/shared/api/apiClient';
 import type { StoreProductsResponse } from './types';
 import { END_POINT } from '@/shared/constants/endpoint';
 
 export const getStoreProducts = () => {
-  return get<StoreProductsResponse>(authApi, END_POINT.STORE.PRODUCTS);
+  return auth.get<StoreProductsResponse>(END_POINT.STORE.PRODUCTS);
 };
