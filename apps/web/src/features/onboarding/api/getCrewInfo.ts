0baa1804ff1731547c6a40f693864c27ac81aa04
sandwe <preengus@gmail.com>
2026-02-09 19:58:39 +0900

    🐛 [fix] 크루 정보 조회시 auth api 인스턴스 사용하도록 변경

--- apps/web/src/features/onboarding/api/getCrewInfo.ts
+++ apps/web/src/features/onboarding/api/getCrewInfo.ts
@@ -1,9 +1,9 @@
-import { base } from '@/shared/api/apiClient';
+import { auth } from '@/shared/api/apiClient';
 import { END_POINT } from '@/shared/constants/endpoint';
 import type { CrewInfoResponseType } from './types';
 
 export const getCrewInfo = (invitationCode: string) => {
-  return base.get<CrewInfoResponseType>(
+  return auth.get<CrewInfoResponseType>(
     END_POINT.ONBOARDING.CREW_INFO(invitationCode)
   );
 };
