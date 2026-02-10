64bbd374d58bfa419ef2387bccb40d4eeb86cc91
sandwe <preengus@gmail.com>
2026-02-10 17:12:09 +0900

    🚚 [rename] AddressFormValues 타입 model로 이동

--- apps/web/src/features/address/ui/index.ts
+++ apps/web/src/features/address/ui/index.ts
@@ -2,5 +2,4 @@ export { AddressCard } from './AddressCard';
 export { AddressEmpty } from './AddressEmpty';
 export { AddressForm } from './AddressForm';
 export { KakaoPostcode } from './KakaoPostcode';
-export type { AddressFormMode, AddressFormValues } from './types';
 export type { AddressFormProps } from './AddressForm';
