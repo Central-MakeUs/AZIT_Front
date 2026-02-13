706415c0485531dde0a2bc0e14b5630258d85716
yummjin <cho4u4o@gmail.com>
2026-02-13 14:35:53 +0900

    ♻️ [refactor] app 레이어 절대경로로 변경

--- apps/web/src/app/routes/config.ts
+++ apps/web/src/app/routes/config.ts
@@ -1,10 +1,9 @@
+import type { RouteConfig } from '@/app/routes/types';
+import { lazyImport } from '@/app/routes/utils';
+
 import { RedirectPage } from '@/pages/auth/ui/RedirectPage';
 import { HomePage } from '@/pages/home/ui/HomePage';
 import { NotFoundPage } from '@/pages/not-found/ui/NotFoundPage';
-
-import type { RouteConfig } from './types';
-import { lazyImport } from './utils';
-
 
 const LoginPage = lazyImport(
   () => import('@/pages/auth/ui/LoginPage'),
