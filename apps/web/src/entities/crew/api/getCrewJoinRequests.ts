645056260045a32ee80a5ede336dd0c3aec8b6dd
yummjin <cho4u4o@gmail.com>
2026-02-21 00:24:08 +0800

    ♻️ [refactor] 크루 가입 요청 및 멤버 조회 도메인 이동

--- apps/web/src/entities/crew/api/getCrewJoinRequests.ts
+++ apps/web/src/entities/crew/api/getCrewJoinRequests.ts
@@ -2,9 +2,9 @@ import { auth } from '@/shared/api/apiClient';
 import type { ApiResponse } from '@/shared/api/baseTypes';
 import { END_POINT } from '@/shared/constants/endpoint';
 
-import type { JoinRequestMemberResult } from '@/entities/user/model';
+import type { JoinRequestMemberResult } from '@/entities/crew/model/crew.types';
 
-export const getJoinRequests = (crewId: number) => {
+export const getCrewJoinRequests = (crewId: number) => {
   return auth.get<ApiResponse<JoinRequestMemberResult[]>>(
     END_POINT.CREW.JOIN_REQUESTS(crewId)
   );
--- apps/web/src/entities/crew/model/crew.types.ts
+++ apps/web/src/entities/crew/model/crew.types.ts
@@ -3,6 +3,7 @@ import type { ApiResponse } from '@/shared/api/baseTypes';
 import type {
   CrewInvitationResponseSchema,
   CrewJoinStatusResponseSchema,
+  JoinRequestMemberResponse,
 } from './crew.model';
 
 export type CrewInfoResult = Required<CrewInvitationResponseSchema>;
@@ -10,3 +11,6 @@ export type CrewInfoResponse = ApiResponse<CrewInfoResult>;
 
 export type CrewJoinStatusResult = Required<CrewJoinStatusResponseSchema>;
 export type CrewJoinStatusResponse = ApiResponse<CrewJoinStatusResult>;
+
+export type JoinRequestMemberResult = Required<JoinRequestMemberResponse>;
+export type MemberRequestItem = JoinRequestMemberResult;
