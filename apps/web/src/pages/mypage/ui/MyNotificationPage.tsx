3eca684ff4d213c008418ad24779aa2b53ca93be
yummjin <cho4u4o@gmail.com>
2026-02-20 22:45:29 +0800

    ♻️ [refactor] 마이페이지 관련 디렉토리 이름 수정 my -> mypage

--- apps/web/src/pages/mypage/ui/MyNotificationPage.tsx
+++ apps/web/src/pages/mypage/ui/MyNotificationPage.tsx
@@ -3,7 +3,7 @@ import { Header } from '@azit/design-system/header';
 import { AppScreen } from '@stackflow/plugin-basic-ui';
 import { useState, useCallback } from 'react';
 
-import * as styles from '@/pages/my/styles/MyNotificationPage.css.ts';
+import * as styles from '@/pages/mypage/styles/MyNotificationPage.css.ts';
 
 import { NotificationSettingsList } from '@/widgets/mypage-notification/ui';
 
