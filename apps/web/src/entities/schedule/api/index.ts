00447e99db9dfa8bbbed2b6b8b02c3b571ff58db
sandwe <preengus@gmail.com>
2026-02-23 02:01:22 +0900

    ✨ [feat] 일정 삭제 기능 및 수정 페이지 네비게이팅 처리

--- apps/web/src/entities/schedule/api/index.ts
+++ apps/web/src/entities/schedule/api/index.ts
@@ -1,5 +1,3 @@
 export { getScheduleList } from './getScheduleList';
 export { getScheduleCalendar } from './getScheduleCalendar';
 export { getScheduleDetail } from './getScheduleDetail';
-export { postParticipateSchedule } from './postParticipateSchedule';
-export { deleteCancelParticipation } from './deleteCancelParticipation';
