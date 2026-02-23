5c5d904934b6526fa21986cbb6a9d0e70098c52b
yummjin <cho4u4o@gmail.com>
2026-02-23 15:00:03 +0900

    💄 [style] 토스트 스타일 vars 사용으로 일관성 개선

--- apps/web/src/app/styles/toast.css.ts
+++ apps/web/src/app/styles/toast.css.ts
@@ -1,11 +1,30 @@
 import { vars } from '@azit/design-system';
 import { globalStyle } from '@vanilla-extract/css';
 
-/* .azit-toast에 디자인시스템 typography body b3 적용 */
+globalStyle(
+  '[data-sonner-toaster][data-x-position="center"][data-y-position="bottom"]',
+  {
+    left: '50% !important',
+    right: 'auto !important',
+    width: 'auto !important',
+  }
+);
+
 globalStyle('.azit-toast', {
-  fontFamily: vars.typography.fontFamily.primary,
-  fontSize: vars.typography.body.b3.fontSize,
-  fontWeight: vars.typography.body.b3.fontWeight,
-  lineHeight: vars.typography.body.b3.lineHeight,
-  letterSpacing: vars.typography.body.b3.letterSpacing,
+  left: '50% !important',
+  right: 'auto !important',
+  transform: 'translateX(-50%) var(--y) !important',
+  marginBottom: '80px',
+  backgroundColor: `${vars.colors.gray80} !important`,
+  borderRadius: '8px !important',
+  border: 'none !important',
+  color: `${vars.colors.white} !important`,
+  width: 'fit-content !important',
+  padding: '8px 24px !important',
+  textWrap: 'nowrap !important',
+  fontFamily: `${vars.typography.fontFamily.primary} !important`,
+  fontSize: `${vars.typography.body.b3.fontSize} !important`,
+  fontWeight: `${vars.typography.body.b3.fontWeight} !important`,
+  lineHeight: `${vars.typography.body.b3.lineHeight} !important`,
+  letterSpacing: `${vars.typography.body.b3.letterSpacing} !important`,
 });
