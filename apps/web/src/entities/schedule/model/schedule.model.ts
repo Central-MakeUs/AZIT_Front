3a40938d168c28b864093eac901a7c887e4c111c
yummjin <cho4u4o@gmail.com>
2026-02-25 13:10:58 +0900

    ✨ [feat] 출석 체크 상태 API 추가 및 쿼리 정의

--- apps/web/src/entities/schedule/model/schedule.model.ts
+++ apps/web/src/entities/schedule/model/schedule.model.ts
@@ -27,3 +27,7 @@ export type ScheduleParticipantsRequest =
   components['schemas']['CursorPageQuery'];
 
 export type ScheduleCheckInRequest = components['schemas']['CheckInRequest'];
+
+export type ScheduleCheckInStatusResponse = RequiredDeep<
+  components['schemas']['CheckInStatusResponse']
+>;
