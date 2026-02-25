ee69503ac616f3958eaf792e28791f620a861541
yummjin <cho4u4o@gmail.com>
2026-02-25 21:00:49 +0900

    ✨ [feat] 과거 날짜 비활성화 기능 추가 및 월 변경 시 바텀시트 닫히는 이슈 해결

--- apps/web/src/widgets/schedule-calendar/ui/ScheduleCalendar.tsx
+++ apps/web/src/widgets/schedule-calendar/ui/ScheduleCalendar.tsx
@@ -13,18 +13,22 @@ interface ScheduleCalendarProps {
   value: Date;
   onChange: (date: Date) => void;
   scheduleData?: ScheduleCalendarItem[];
+  isPastDateDisabled?: boolean;
+  onMonthChange?: (date: Date) => void;
 }
 
 export function ScheduleCalendar({
   value,
   onChange,
   scheduleData,
+  isPastDateDisabled = false,
+  onMonthChange,
 }: ScheduleCalendarProps) {
   const handlePreviousMonth = () => {
-    onChange(dayjs(value).subtract(1, 'month').toDate());
+    onMonthChange?.(dayjs(value).subtract(1, 'month').toDate());
   };
   const handleNextMonth = () => {
-    onChange(dayjs(value).add(1, 'month').toDate());
+    onMonthChange?.(dayjs(value).add(1, 'month').toDate());
   };
 
   const activeStartDate = dayjs(value).startOf('month').toDate();
@@ -39,12 +43,14 @@ export function ScheduleCalendar({
           <button
             className={styles.calendarHeaderButton}
             onClick={handlePreviousMonth}
+            type="button"
           >
             <ChevronLeftIcon size={24} style={{ color: vars.colors.blue80 }} />
           </button>
           <button
             className={styles.calendarHeaderButton}
             onClick={handleNextMonth}
+            type="button"
           >
             <ChevronRightIcon size={24} style={{ color: vars.colors.blue80 }} />
           </button>
@@ -62,6 +68,18 @@ export function ScheduleCalendar({
         }}
         formatShortWeekday={(_, date) => formatDate(date, 'ddd').toUpperCase()}
         formatDay={(_, date) => formatDate(date, 'D')}
+        tileClassName={({ date }) => {
+          if (dayjs(date).isBefore(dayjs().startOf('day'))) {
+            return isPastDateDisabled ? styles.pastDateTile : '';
+          }
+          return '';
+        }}
+        tileDisabled={({ date }) => {
+          if (isPastDateDisabled) {
+            return dayjs(date).isBefore(dayjs().startOf('day'));
+          }
+          return false;
+        }}
         tileContent={({ date }: { date: Date }) => {
           const schedule = scheduleData?.find((item) =>
             dayjs(item.date).isSame(date, 'day')
