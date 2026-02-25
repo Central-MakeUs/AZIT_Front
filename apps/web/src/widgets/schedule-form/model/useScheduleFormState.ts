0072901f151d598e2f57a0e7e58f13991cd685cd
sandwe <preengus@gmail.com>
2026-02-26 01:51:33 +0900

    ✨ [feat] 일정 폼 과거시간 설정 관련 토스트 UI 처리

--- apps/web/src/widgets/schedule-form/model/useScheduleFormState.ts
+++ apps/web/src/widgets/schedule-form/model/useScheduleFormState.ts
@@ -1,12 +1,13 @@
-// useScheduleFormValues.ts
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 import {
+  isScheduleDateTimeInPast,
   isScheduleFormValid,
   type ScheduleFormValues,
 } from '@/widgets/schedule-form/model/scheduleForm';
 
 import { useScheduleLocationSelectionStore } from '@/shared/store/scheduleLocationSelection';
+import { toastError } from '@/shared/ui/toast';
 
 export const useScheduleFormState = (initialValues: ScheduleFormValues) => {
   const [formValues, setFormValues] =
@@ -28,11 +29,24 @@ export const useScheduleFormState = (initialValues: ScheduleFormValues) => {
     clearLocation();
   }, [selectedLocation, clearLocation]);
 
-  const isValid = isScheduleFormValid(formValues);
+  const validateForm = useCallback(() => {
+    const { date, amPm, hour, minute } = formValues;
+
+    if (isScheduleDateTimeInPast(date, amPm, hour, minute)) {
+      toastError('이미 지난 시간은 설정할 수 없습니다');
+      return false;
+    }
+
+    if (!isScheduleFormValid(formValues)) {
+      return false;
+    }
+
+    return true;
+  }, [formValues]);
 
   return {
     formValues,
     setFormValues,
-    isValid,
+    validateForm,
   };
 };
