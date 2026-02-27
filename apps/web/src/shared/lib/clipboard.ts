89c3197e9c9affcda72d8ca250fc9538813ee151
yummjin <cho4u4o@gmail.com>
2026-02-27 19:27:15 +0900

    🐛 [fix] 부끄러운 실수

--- apps/web/src/shared/lib/clipboard.ts
+++ apps/web/src/shared/lib/clipboard.ts
@@ -4,8 +4,8 @@ import { toastSuccess } from '../ui/toast';
 export const copyToClipboard = (text: string, label: string) => {
   navigator.clipboard.writeText(text);
   if (hasFinalConsonant(label)) {
-    toastSuccess(`${label}가 복사되었습니다.`);
-  } else {
     toastSuccess(`${label}이 복사되었습니다.`);
+  } else {
+    toastSuccess(`${label}가 복사되었습니다.`);
   }
 };
