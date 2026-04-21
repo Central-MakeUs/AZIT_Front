12884ae6381e8c8f8b4982dc219a3a56c0f32e85
sandwe <preengus@gmail.com>
2026-04-22 00:50:44 +0900

    feat: 프로필 이미지 s3 업로드 기능 구현

--- apps/web/src/shared/api/handlers/index.ts
+++ apps/web/src/shared/api/handlers/index.ts
@@ -1,2 +1,4 @@
 export { postLogout } from '@/shared/api/handlers/postLogout';
+export { postPresignedUrl } from '@/shared/api/handlers/postPresignedUrl';
 export { postReissueToken } from '@/shared/api/handlers/postReissueToken';
+export { putS3Upload } from '@/shared/api/handlers/putS3Upload';
