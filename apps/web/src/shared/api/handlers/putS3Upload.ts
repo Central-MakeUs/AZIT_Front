890829fa6a4ff8cf890d91226a8b22e1bb9e5f2d
sandwe <preengus@gmail.com>
2026-04-22 00:50:44 +0900

    feat: 프로필 이미지 s3 업로드 기능 구현

---
+++ apps/web/src/shared/api/handlers/putS3Upload.ts
@@ -0,0 +1,12 @@
+export const putS3Upload = async (
+  presignedUrl: string,
+  blob: Blob,
+  mimeType: string
+) => {
+  const res = await fetch(presignedUrl, {
+    method: 'PUT',
+    body: blob,
+    headers: { 'Content-Type': mimeType },
+  });
+  if (!res.ok) throw new Error('S3 업로드 실패');
+};
