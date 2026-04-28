1354f2670df483438a2a9581c5b26f2a9d8d7be5
sandwe <preengus@gmail.com>
2026-04-28 17:35:08 +0900

    🔧 [chore] subject-case 규칙 비활성화

--- commitlint.config.js
+++ commitlint.config.js
@@ -15,5 +15,6 @@ module.exports = {
     'subject-empty': [2, 'never'],
     'subject-full-stop': [2, 'never', '.'],
     'header-max-length': [2, 'always', 100],
+    'subject-case': [0],
   },
 };
