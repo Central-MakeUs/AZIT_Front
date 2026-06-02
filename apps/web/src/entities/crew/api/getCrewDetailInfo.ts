c7c0b7f7f825e79ba29a81f5d525a5b5e52f6573
sandwe <preengus@gmail.com>
2026-06-02 15:44:28 +0900

    🎨 [fix] prettier 에러

--- apps/web/src/entities/crew/api/getCrewDetailInfo.ts
+++ apps/web/src/entities/crew/api/getCrewDetailInfo.ts
@@ -3,7 +3,6 @@ import type { ApiResponse } from '@/shared/api/baseTypes';
 import type { CrewDetailInfoResult } from '@/shared/api/models/crew';
 import { END_POINT } from '@/shared/constants/endpoint';
 
-
 export const getCrewDetailInfo = (crewId: number) => {
   return auth.get<ApiResponse<CrewDetailInfoResult>>(
     END_POINT.CREW.UPDATE_INFO(crewId)
