75ba1eaeda185da7af7fd0cdf0582007d3d22d20
sandwe <preengus@gmail.com>
2026-06-16 22:36:55 +0900

    ♻️ [refactor] ScheduleList 빈 상태 스타일을 각 페이지로 분리

--- apps/web/src/entities/schedule/styles/ScheduleList.css.ts
+++ apps/web/src/entities/schedule/styles/ScheduleList.css.ts
@@ -1,4 +1,3 @@
-import { typography, vars } from '@azit/design-system';
 import { style } from '@vanilla-extract/css';
 
 export const listContainer = style({
@@ -6,28 +5,4 @@ export const listContainer = style({
   flexDirection: 'column',
   gap: '10px',
   width: '100%',
-});
-
-export const emptyContainer = style({
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  justifyContent: 'center',
-  width: '100%',
-  height: 'fit-content',
-  minHeight: '220px',
-  padding: '30px 0px 100px 0px',
-  gap: '16px',
-});
-
-export const emptyText = style([
-  typography.body.b2,
-  {
-    color: vars.colors.gray60,
-  },
-]);
-
-export const addScheduleButton = style({
-  width: 'fit-content',
-  padding: '0 30px',
 });
