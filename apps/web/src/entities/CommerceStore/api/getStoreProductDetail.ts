aa53503888d2be7d64d9161e9e31ee9b19664420
yummjin <cho4u4o@gmail.com>
2026-06-27 23:25:03 +0900

    ♻️ [refactor] entities 디렉토리 Commerce prefix 및 PascalCase로 일원화

--- apps/web/src/entities/CommerceStore/api/getStoreProductDetail.ts
+++ apps/web/src/entities/CommerceStore/api/getStoreProductDetail.ts
@@ -1,7 +1,7 @@
 import { auth } from '@/shared/api/apiClient';
 import { END_POINT } from '@/shared/constants/endpoint';
 
-import type { StoreProductDetailResponse } from '@/entities/store/model';
+import type { StoreProductDetailResponse } from '@/entities/CommerceStore/model';
 
 export const getStoreProductDetail = (productId: string) => {
   return auth.get<StoreProductDetailResponse>(
