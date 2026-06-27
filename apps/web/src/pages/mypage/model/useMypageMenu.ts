187f389e86fde4c548e7bec519667997be183793
yummjin <cho4u4o@gmail.com>
2026-06-27 22:54:14 +0900

    ✨ [feat] MyPage 플랫 구조로 마이그레이션 및 메뉴 로직 인라인 통합

--- apps/web/src/pages/mypage/model/useMypageMenu.ts
+++
@@ -1,8 +0,0 @@
-import { useFlow } from '@/app/routes/stackflow';
-
-import { getMypageMenu } from '@/pages/mypage/config/menu';
-
-export const useMypageMenu = () => {
-  const { push } = useFlow();
-  return getMypageMenu(push);
-};
